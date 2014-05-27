71c120b799fa142b4f967aa8f94c08c68b2efd1f
Ben Pellow <bpellow@walmartlabs.com>
2014-05-27 14:06:16 -0700

    Initial Commit

---
+++ ToDoList/WMTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WMTAppDelegate.h
+//  ToDoList
+//
+//  Created by Ben Pellow on 5/27/14.
+//  Copyright (c) 2014 Walmart Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Ben Pellow on 5/27/14.
+//  Copyright (c) 2014 Walmart Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WMTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WMTAppDelegate class]));
+    }
+}

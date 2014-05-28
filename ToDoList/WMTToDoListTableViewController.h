88bd662e9274b4d952f48f46b03ca4381049dc95
Ben Pellow <bpellow@walmartlabs.com>
2014-05-28 11:41:02 -0700

    custom Table View Controller for table view :cow:

---
+++ ToDoList/WMTToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMTToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Ben Pellow on 5/28/14.
+//  Copyright (c) 2014 Walmart Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMTToDoListTableViewController : UITableViewController
+
+@end
